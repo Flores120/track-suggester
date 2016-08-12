23f64e8b60205a8a6d43a9470d513b85d2b84807
Alvaro Flores <alvaroflores@Alvaros-MacBook-Pro.local>
2016-08-12 09:55:03 -0700

    added some if and else if statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,20 @@
-// var art = $("input:radio[name=art]:checked").val();
-// **************
+var css = 0
+var php = 0
+var c = 0
+
+var firstQ = function(an1) {
+    if (an1 === 'agree') {
+    css ++;
+  } else if (an1 === 'neautral') {
+    php ++;
+  } else if (an1 === 'disagree') {
+    c ++;
+  } return ( css, php, c);
+}
+// ------------->front end work<---------------------
 $(function() {
   $('#submitButton').click(function(event) {
+    alert("hello");
     var an1 = $('input[name=q1]:checked').val();
     var an2 = $('input[name=q2]:checked').val();
     var an3 = $('input[name=q3]:checked').val();
