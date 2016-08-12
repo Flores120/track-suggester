ec6af54cab10a0aefe8885923cbf54f8158975c6
Alvaro Flores <alvaroflores@Alvaros-MacBook-Pro.local>
2016-08-12 10:21:54 -0700

    added values to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -9,18 +9,53 @@ var firstQ = function(an1) {
     php ++;
   } else if (an1 === 'disagree') {
     c ++;
-  } return ( css, php, c);
+  } return css, php, c;
+}
+var secQ = function(an2) {
+    if (an2 === 'agree') {
+    c ++;
+  } else if (an2 === 'neautral') {
+    php ++;
+  } else if (an2 === 'disagree') {
+    css ++;
+  } return css, php, c;
+}
+var thirdQ = function(a3) {
+    if (a3 === 'agree') {
+    php ++;
+  } else if (a3 === 'neautral') {
+    c ++;
+  } else if (a3 === 'disagree') {
+    css ++;
+  } return css, php, c;
+}
+var fourthQ = function(an4) {
+    if (an4 === 'agree') {
+    css ++;
+  } else if (an4 === 'neautral') {
+    c ++;
+  } else if (an4 === 'disagree') {
+    php ++;
+  } return css, php, c;
+}
+var fifthQ = function(an5) {
+    if (an5 === 'agree') {
+    php ++;
+  } else if (an5 === 'neautral') {
+    c ++;
+  } else if (an5 === 'disagree') {
+    css ++;
+  } return css, php, c;
 }
 // ------------->front end work<---------------------
 $(function() {
   $("#submit").click(function(event) {
-    alert("hello");
     var an1 = $('input[name=q1]:checked').val();
     var an2 = $('input[name=q2]:checked').val();
     var an3 = $('input[name=q3]:checked').val();
     var an4 = $('input[name=q4]:checked').val();
     var an5 = $('input[name=q5]:checked').val();
+    return (firstQ(an1) + secQ(an2) + thirdQ(an3) + fourthQ(an4) + fifthQ(an5));
     event.preventDefault();
-    alert("hey it works");
   });
 });
