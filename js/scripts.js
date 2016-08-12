47c5caed595653cfcaff82e4273fb1acef20a603
Alvaro Flores <alvaroflores@Alvaros-MacBook-Pro.local>
2016-08-12 11:08:39 -0700

    tweaked more of the javascript

--- js/scripts.js
+++ js/scripts.js
@@ -4,48 +4,64 @@ var c = 0
 
 var firstQ = function(an1) {
     if (an1 === 'agree') {
-    css ++;
-  } else if (an1 === 'neautral') {
-    php ++;
+    css += 3;
+  } else if (an1 === 'neutral') {
+    php += 2;
   } else if (an1 === 'disagree') {
     c ++;
   } return css, php, c;
 }
 var secQ = function(an2) {
     if (an2 === 'agree') {
-    c ++;
-  } else if (an2 === 'neautral') {
-    php ++;
+    c += 3;
+  } else if (an2 === 'neutral') {
+    php += 2;
   } else if (an2 === 'disagree') {
     css ++;
   } return css, php, c;
 }
 var thirdQ = function(a3) {
     if (a3 === 'agree') {
-    php ++;
-  } else if (a3 === 'neautral') {
-    c ++;
+    php += 3;
+  } else if (a3 === 'neutral') {
+    c += 2;
   } else if (a3 === 'disagree') {
     css ++;
   } return css, php, c;
 }
 var fourthQ = function(an4) {
     if (an4 === 'agree') {
-    css ++;
-  } else if (an4 === 'neautral') {
-    c ++;
+    css += 3;
+  } else if (an4 === 'neutral') {
+    c += 2;
   } else if (an4 === 'disagree') {
     php ++;
   } return css, php, c;
 }
 var fifthQ = function(an5) {
     if (an5 === 'agree') {
-    php ++;
-  } else if (an5 === 'neautral') {
-    c ++;
+    php += 3;
+  } else if (an5 === 'neutral') {
+    c += 2;
   } else if (an5 === 'disagree') {
     css ++;
   } return css, php, c;
+}
+var sixthQ = function(an6) {
+    if (an6 === 'agree') {
+    c += 3;
+  } else if (an6 === 'neutral') {
+    php += 2;
+  } else if (an6 === 'disagree') {
+    css ++;
+  } return css, php, c;
+}
+var results = function() {
+  if (css > c && php) {
+  console.log('youre css');
+} else if (c > css && php) {
+} else if php > css && c) {
+} return
 }
 // ------------->front end work<---------------------
 $(function() {
@@ -55,7 +71,8 @@ $(function() {
     var an3 = $('input[name=q3]:checked').val();
     var an4 = $('input[name=q4]:checked').val();
     var an5 = $('input[name=q5]:checked').val();
-    return (firstQ(an1) + secQ(an2) + thirdQ(an3) + fourthQ(an4) + fifthQ(an5));
+    var an6 = $('input[name=q6]:checked').val();
+    return (firstQ(an1) + secQ(an2) + thirdQ(an3) + fourthQ(an4) + fifthQ(an5) + sixthQ(an6));
     event.preventDefault();
   });
 });
