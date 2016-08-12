0b1d0962878cc9dfde02c41d9cfde1b8655b7945
Alvaro Flores <alvaroflores@Alvaros-MacBook-Pro.local>
2016-08-12 09:05:31 -0700

    inserted some javascript and jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-var art = $("input:radio[name=art]:checked").val();
+// var art = $("input:radio[name=art]:checked").val();
+// **************
+$(function() {
+  $('#submitButton').click(function(event) {
+    var an1 = $('input[name=q1]:checked').val();
+    var an2 = $('input[name=q2]:checked').val();
+    var an3 = $('input[name=q3]:checked').val();
+    var an4 = $('input[name=q4]:checked').val();
+    var an5 = $('input[name=q5]:checked').val();
+    event.preventDefault();
+    console.log();
+  });
+});
